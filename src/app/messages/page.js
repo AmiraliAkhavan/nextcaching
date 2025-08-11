37fdc1631826a722d9efc141bf4d2f70601375b0
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2025-08-11 15:16:21 +0330

    connection setup

--- src/app/messages/page.js
+++ src/app/messages/page.js
@@ -1,11 +1,20 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
+import { connection } from "next/server";
+
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic"; // same as cache:no-store
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
+  await connection();
+  const response = await fetch(
+    "http://localhost:8080/messages"
+    // , {
+    // next: {
+    //   revalidate: 3,
+    // },
+    // cache: "no-store", nextjs 15 default
+    // }
+  );
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
