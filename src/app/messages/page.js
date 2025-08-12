dc9930e943dde8acceeb0bb491c8f9712da1d0a4
AmiraliAkhavan <111018923+AmiraliAkhavan@users.noreply.github.com>
2025-08-13 00:41:57 +0330

    next cache setup

--- src/app/messages/page.js
+++ src/app/messages/page.js
@@ -1,21 +1,23 @@
 import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
 import { connection } from "next/server";
 
 // export const revalidate = 5;
 // export const dynamic = "force-dynamic"; // same as cache:no-store
 
 export default async function MessagesPage() {
-  await connection();
-  const response = await fetch(
-    "http://localhost:8080/messages"
-    // , {
-    // next: {
-    //   revalidate: 3,
-    // },
-    // cache: "no-store", nextjs 15 default
-    // }
-  );
-  const messages = await response.json();
+  // await connection();
+  // const response = await fetch(
+  // "http://localhost:8080/messages"
+  // , {
+  // next: {
+  //   revalidate: 3,
+  // },
+  // cache: "no-store", nextjs 15 default
+  // }
+  // );
+
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
